329f980a0e87960d1e442861681084425aa3a37d
Deonte Waller <dwallerofficial@gmail.com>
2021-05-30 21:32:21 -0400

    Local Notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -3,6 +3,7 @@ import { Text, View, ScrollView, StyleSheet, Picker, Switch, Button, Modal } fro
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as Animatable from 'react-native-animatable';
 import { Alert } from 'react-native';
+import * as Notifications from 'expo-notifications';
 
 class Reservation extends Component {
 
@@ -42,7 +43,10 @@ class Reservation extends Component {
         },
         {
           text: 'OK',
-          onPress: () => this.resetForm()
+          onPress: () => {
+            this.presentLocalNotification(this.state.date.toLocaleDateString('en-us'));
+            this.resetForm();
+          }
         }
       ]
     );
@@ -55,6 +59,36 @@ class Reservation extends Component {
       date: new Date(),
       showCalendar: false
     });
+  }
+
+  async presentLocalNotification(date) {
+
+    function sendNotification() {
+      Notifications.setNotificationHandler({
+        handleNotification: async () => ({
+          shouldShowAlert: true
+        })
+      });
+
+      Notifications.scheduleNotificationAsync({
+        content: {
+          title: 'Your Campsite Reservation Search',
+          body: `Search for ${date} requested`
+        },
+        trigger: null
+      });
+    }
+
+    let permissions = await Notifications.getPermissionsAsync();
+
+    if (!permissions.granted) {
+      permissions = await Notifications.requestPermissionsAsync();
+    }
+
+    if (permissions.granted) {
+      sendNotification();
+    }
+
   }
 
   // RENDER & VIEW
